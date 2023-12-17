4eba8f6475de5cb677d5d459d35c1c7746564718
Anna Haig <anna.haig@protonmail.com>
2023-12-17 23:29:31 +0000

    added library to cover precedence

--- src/parser2.rs
+++ src/parser2.rs
@@ -4,7 +4,7 @@ use nom::{
     branch::alt,
     IResult,
     combinator::{
-        opt, map,
+        opt, map, fail, map_res,
     },
     bytes::complete::{
         tag_no_case,
@@ -14,8 +14,9 @@ use nom::{
     character::complete::{
         multispace0,
         multispace1,
-    }, multi::many1
+    }, multi::many1, sequence::delimited
 };
+use nom_7_precedence::{precedence, binary_op, Assoc, unary_op, Operation};
 
 pub fn parse_program(input: &str) -> IResult<&str, Program> {
     let (input, statements) = many1(parse_statment)(input)?;
@@ -33,37 +34,37 @@ fn parse_statment(input: &str) -> IResult<&str, Box<dyn Statement>> {
 }
 
 fn parse_let_statement(input: &str) -> IResult<&str, Box<dyn Statement>> {
-    let (input, _) = tag_no_case("let")(input)?;
+    let (input, _) = tag("let")(input)?;
     let (input, _) = multispace1(input)?;
     let (input, mutable) = opt(tag_no_case("mut"))(input)?;
     let (input, _) = multispace0(input)?;
-    let (input, name) = parse_identifier(input)?;
+    let (input, name) = parse_identifier_literal(input)?;
     let (input, _) = multispace0(input)?;
-    let (input, _) = tag_no_case("=")(input)?;
+    let (input, _) = tag("=")(input)?;
     let (input, _) = multispace0(input)?;
     let (input, value) = parse_expression(input)?;
     let (input, _) = multispace0(input)?;
     
-    let (input, _) = tag_no_case(";")(input)?;
+    let (input, _) = tag(";")(input)?;
     Ok((input, Box::new(LetStatement 
         { mutable: mutable.is_some(), name, value })))
 }
 
 fn parse_return_statement(input: &str) -> IResult<&str, Box<dyn Statement>> {
-    let (input, _) = tag_no_case("return")(input)?;
+    let (input, _) = tag("return")(input)?;
     let (input, _) = multispace1(input)?;
     let (input, value) = parse_expression(input)?;
     Ok((input, Box::new(ReturnStatement { value })))
 }
 
-fn parse_identifier(input: &str) -> IResult<&str, IdentifierLiteral> {
+fn parse_identifier_literal(input: &str) -> IResult<&str, IdentifierLiteral> {
     let (input, name) = take_while1(|c: char| c.is_alphanumeric())(input)?;
     Ok((input, IdentifierLiteral { name: name.to_string() }))
 }
 
-fn parse_integer(input: &str) -> IResult<&str, IntegerLiteral> {
+fn parse_integer_literal(input: &str) -> IResult<&str, IntegerLiteral> {
     let (input, value) = take_while1(|c: char| c.is_numeric() || c == '_')(input)?;
-    let (input, _) = tag_no_case("i")(input)?;
+    let (input, _) = tag("i")(input)?;
     let (input, length) = take_while(|c: char| c.is_numeric() || c == '_')(input)?;
     Ok((input, IntegerLiteral {
         value: value.to_string(),
@@ -71,7 +72,7 @@ fn parse_integer(input: &str) -> IResult<&str, IntegerLiteral> {
     }))
 }
 
-fn parse_boolean(input: &str) -> IResult<&str, BooleanLiteral> {
+fn parse_boolean_literal(input: &str) -> IResult<&str, BooleanLiteral> {
     let (input, exp) = alt((
         map(tag("true"), |_| BooleanLiteral { value: true }),
         map(tag("false"), |_| BooleanLiteral { value: false })
@@ -80,13 +81,48 @@ fn parse_boolean(input: &str) -> IResult<&str, BooleanLiteral> {
 }
 
 fn parse_expression(input: &str) -> IResult<&str, Box<dyn Expression>> {
-    let (input, exp) = alt((
-        map(parse_integer, |e| Box::new(e) as Box<dyn Expression>),
-        map(parse_boolean, |e| Box::new(e) as Box<dyn Expression>),
-        map(parse_identifier, |e| Box::new(e) as Box<dyn Expression>),
-    ))(input)?;
-    Ok((input, exp))
+    precedence(
+alt((
+            unary_op(1, tag("-")),
+            unary_op(1, tag("!")),
+        )),
+        fail,
+        alt((
+            binary_op(2, Assoc::Left, tag("*")),
+            binary_op(2, Assoc::Left, tag("/")),
+            binary_op(3, Assoc::Left, tag("+")),
+            binary_op(3, Assoc::Left, tag("-")),
+            binary_op(4, Assoc::Left, tag("==")),
+            binary_op(4, Assoc::Left, tag("!=")),
+            binary_op(4, Assoc::Left, tag(">")),
+            binary_op(4, Assoc::Left, tag(">=")),
+            binary_op(4, Assoc::Left, tag("<")),
+            binary_op(4, Assoc::Left, tag("<=")),
+        )),
+        alt((
+            delimited(tag("("), parse_expression, tag(")")),
+            map(parse_integer_literal, |i| Box::new(i) as Box<dyn Expression>),
+            map(parse_boolean_literal, |b| Box::new(b) as Box<dyn Expression>),
+            map(parse_identifier_literal, |i| Box::new(i) as Box<dyn Expression>),
+       )),
+        |op: Operation<&str, &str, &str, Box<dyn Expression>>| { //evaluating the expression step by step
+            match op {
+                Operation::Prefix(op, o) => Ok(Box::new(PrefixExpression {
+                    operator: op.to_string(),
+                    right: o,
+                })),
+                Operation::Binary(lhs, op, rhs) => Ok(Box::new(InfixExpression {
+                    operator: op.to_string(),
+                    left: lhs,
+                    right: rhs,
+                })),
+                _ => Err("Invalid combination"),
+            }
+          }
+    )(input)
 }
+
+// =====
 
 #[derive(Debug)]
 pub struct IdentifierLiteral {
@@ -110,6 +146,27 @@ pub struct BooleanLiteral {
 
 impl Expression for BooleanLiteral {}
 
+// =====
+
+#[derive(Debug)]
+pub struct PrefixExpression {
+    operator: String,
+    right: Box<dyn Expression>,
+}
+
+impl Expression for PrefixExpression {}
+
+#[derive(Debug)]
+pub struct InfixExpression {
+    operator: String,
+    left: Box<dyn Expression>,
+    right: Box<dyn Expression>,
+}
+
+impl Expression for InfixExpression {}
+
+// =====
+
 #[derive(Debug)]
 pub struct LetStatement {
     name: IdentifierLiteral,
@@ -126,10 +183,14 @@ pub struct ReturnStatement {
 
 impl Statement for ReturnStatement {}
 
+// =====
+
 #[derive(Debug)]
 pub struct Program {
     statements: Vec<Box<dyn Statement>>,
 }
+
+// =====
 
 trait Expression: Debug {
     
